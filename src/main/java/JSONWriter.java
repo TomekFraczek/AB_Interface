49377dae03f8897b85ff44e443804b2f00147c43
Tomek <tomaszmf95@yahoo.com>
2018-02-12 15:34:40 -0800

    Created JSON Writer

---
+++ src/main/java/JSONWriter.java
@@ -0,0 +1,48 @@
+import com.intuit.developer.sampleapp.oauth2.controller.QBOController;
+import org.apache.log4j.Logger;
+import org.json.JSONObject;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class JSONWriter {
+    /**
+     * Class to simplify writing out JSON files and safely handle Exceptions
+     */
+
+    // Get access to the console logger
+    private static final Logger logger = Logger.getLogger(QBOController.class);
+
+    // The directory for the fileWriter to write all files to
+    private String directoryPath;
+
+
+    public JSONWriter(String dirPath) {
+        this.directoryPath = dirPath;
+    }
+
+    // Convert a JSON object to a pretty-printed string and write it out to the file
+    public void write(String filename, JSONObject obj) {
+
+        // Combine the given file name with the directory
+        String fullFilename = this.directoryPath + filename + ".json";
+
+        // Perform the write, logging wither success of failure
+        try (FileWriter file = new FileWriter(fullFilename)) {
+            file.write(obj.toString(2));
+            logger.info("Successfully Copied JSON Object to File...");
+        } catch (IOException e) {
+            logger.warn("Object write FAILED!");
+        }
+    }
+
+    // Write the given string to a file, ensuring that it is pretty-printed
+    public void write(String filename, String jsonString) {
+
+        // Convert the jsonString to a json object so it can get pretty printed
+        JSONObject obj = new JSONObject(jsonString);
+
+        // The basic write method can handle it from here on out
+        this.write(filename, obj);
+    }
+}
