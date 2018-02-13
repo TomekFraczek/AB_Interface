8cedaac86b1241ca870c3d5a640c8d71b9fe47ab
Tomek <tomaszmf95@yahoo.com>
2018-02-12 16:32:25 -0800

    Fixed placement of Application

--- src/main/java/nomad/Application.java
+++ src/main/java/nomad/Application.java
@@ -1,3 +1,5 @@
+package nomad;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
