dc1ed35756806b68a911fdcbe054ec95d3a47c5a
Tomek <tomaszmf95@yahoo.com>
2018-02-13 09:49:49 -0800

    Separated the GET response into it's own method

--- src/main/java/nomad/controller/QueryController.java
+++ src/main/java/nomad/controller/QueryController.java
@@ -57,9 +57,23 @@ public class QueryController {
 
         String queryEndpoint = String.format("%s/v3/company/%s/%s", oAuth2Configuration.getAccountingAPIHost(), realmId, "query?query=select%20%2a%20from%20Invoice&minorversion=4");
 
+        // Get the result of the query request
+        JSONObject result = doQueryGet(session, queryEndpoint);
+
+        // We've hijacked the above request to get the shipments data. Now we write it out to a file for later work
+        String filename = "Shipments";
+        JSONWriter writer = new JSONWriter();
+        writer.datedWrite(filename, result);
+
+        return result.toString();
+    }
+
+    /** Performs a GET request to execute a query, and returns the result as a JSONObject */
+    private JSONObject doQueryGet(HttpSession session, String queryEndpoint) {
+
         String failureMsg="Failed";
 
-        // Create the HTTP GET request to be executed
+        // Turn the endpoint into a valid HTTP GET request
         HttpGet queryRequest = new HttpGet(queryEndpoint);
         queryRequest.setHeader("Accept", "application/json");
         String accessToken = (String)session.getAttribute("access_token");
@@ -69,50 +83,44 @@ public class QueryController {
             HttpResponse response = CLIENT.execute(queryRequest);
 
             logger.info("Response Code : "+ response.getStatusLine().getStatusCode());
-            
+
             /*
-             * Handle 401 status code - 
+             * Handle 401 status code -
              * If a 401 response is received, refresh tokens should be used to get a new access token,
              * and the API call should be tried again.
              */
             if (response.getStatusLine().getStatusCode() == 401) {
                 StringBuffer result = httpHelper.getResult(response);
                 logger.debug("raw result for 401 companyInfo= " + result);
-                
+
                 //refresh tokens
                 logger.info("received 401 during companyinfo call, refreshing tokens now");
                 BearerTokenResponse bearerTokenResponse = refreshTokenService.refresh(session);
                 session.setAttribute("access_token", bearerTokenResponse.getAccessToken());
                 session.setAttribute("refresh_token", bearerTokenResponse.getRefreshToken());
-                
+
                 //call company info again using new tokens
                 logger.info("calling companyinfo using new tokens");
                 queryRequest.setHeader("Authorization","Bearer " + bearerTokenResponse.getAccessToken());
                 response = CLIENT.execute(queryRequest);
-            }  
-            
+            }
+
             // Pass up a serious error (200 type) so it can be displayed
             if (response.getStatusLine().getStatusCode() != 200){
                 logger.info("failed getting companyInfo");
-                return new JSONObject().put("response",failureMsg).toString();
+                return new JSONObject().put("response",failureMsg);
             }
 
             // Extract (and log) the query result from the HTTP response
             StringBuffer result = httpHelper.getResult(response);
             logger.debug("raw result for companyInfo= " + result);
 
-
-            // We've hijacked the above request to get the shipments data. Now we write it out to a file for later work
-            String filename = "Shipments";
-            JSONWriter writer = new JSONWriter();
-            writer.datedWrite(filename, result);
-
-            return result.toString();
+            return new JSONObject(result);
 
         // Catch any unexpected exceptions and pass them up to the log and for display
         } catch (Exception ex) {
             logger.error("Exception while getting company info ", ex);
-            return new JSONObject().put("response",failureMsg).toString();
+            return new JSONObject().put("response", failureMsg);
         }
     }
 
