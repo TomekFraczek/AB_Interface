3f40a8a9547e893c31a21d32e22d4532da0537f2
Tomek <tomaszmf95@yahoo.com>
2018-02-12 16:03:01 -0800

    Made the data output path a property

--- src/main/java/JSONWriter.java
+++ src/main/java/JSONWriter.java
@@ -2,25 +2,30 @@ import controller.QBOController;
 
 import org.apache.log4j.Logger;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 import java.io.FileWriter;
 import java.io.IOException;
 
+
+@Configuration
+@PropertySource(value="classpath:/application.properties", ignoreResourceNotFound=true)
 public class JSONWriter {
     /**
      * Class to simplify writing out JSON files and safely handle Exceptions
      */
 
+    @Autowired
+    Environment env;
+
     // Get access to the console logger
     private static final Logger logger = Logger.getLogger(QBOController.class);
 
     // The directory for the fileWriter to write all files to
-    private String directoryPath;
-
-
-    public JSONWriter(String dirPath) {
-        this.directoryPath = dirPath;
-    }
+    private String directoryPath = env.getProperty("datapath");
 
     // Convert a JSON object to a pretty-printed string and write it out to the file
     public void write(String filename, JSONObject obj) {
